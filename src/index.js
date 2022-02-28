2a3725d92d2788a90cc8836f0d65b969b499e3ae
TrentHurlbut <trent.hurlbut@gmail.com>
2022-02-28 15:16:45 -0800

    Set up new root field and new resolver for list of links.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('apollo-server');
 
+//root fields
 const typeDefs = `
   type Query {
     info: String!
@@ -16,13 +17,25 @@ const typeDefs = `
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
   },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+  }
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
 
 server
   .listen()
